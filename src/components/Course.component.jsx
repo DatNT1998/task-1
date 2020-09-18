756866566bffed10e398b0b9d4677221785d8c99
DatNT1998 <44127716+DatNT1998@users.noreply.github.com>
2020-09-18 11:04:46 +0700

    task-1

--- src/components/Course.component.jsx
+++ src/components/Course.component.jsx
@@ -1,24 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+//https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=PLkY6Xj8Sg8-vV5kALCOT0LShKc6mVFBvW&key=[YOUR_API_KEY] 
 
 function Course(props) {
     const courseName = props.match.params.coursename;
 
-    const courses = {
-        reactjs: [
-            { title: "react js title 1", vid: "o8jEBYOu9dA" },
-            { title: "react js title 2", vid: "FOSqMousEYs" },
-            { title: "react js title 3", vid: "LJoGgn3K76I" },
-        ],
-        nodejs: [
-            { title: "node js title 1", vid: "o8jEBYOu9dA" },
-            { title: "node js title 2", vid: "FOSqMousEYs" },
-            { title: "node js title 3", vid: "LJoGgn3K76I" },
-        ],
+    const [courses, setCourses] = useState([]);
 
-    }
+    useEffect(() => {
 
-    const [title, setTiTle] = useState(courses[courseName][0].title);
-    const [vid, setVid] = useState(courses[courseName][0].vid);
+        let playListId = ""
+        if (courseName === 'reactjs') {
+            playListId = 'PLkY6Xj8Sg8-vV5kALCOT0LShKc6mVFBvW'
+        } else {
+            playListId = 'PLkY6Xj8Sg8-s-m-qFBQFoeNSfpCTBiwMU'
+        }
+
+        fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=${playListId}&key=AIzaSyBO28gdEoriekku32-3fqnAiX4bLWefzp0`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const result = data.items.map(item => {
+                    return { title: item.snippet.title, vid: item.contentDetails.videoId }
+                })
+                console.log(result, 'day ne')
+                setCourses(result)
+                setTiTle(result[0].title);
+                setVid(result[0].vid);
+            })
+    }, [])
+
+    const [title, setTiTle] = useState("");
+    const [vid, setVid] = useState("");
+    const [counter, setCounter] = useState(0);
 
     const renderVideo = () => {
         return (
@@ -28,19 +44,45 @@ function Course(props) {
         )
     }
     return (
-        <div >
-            <h1> {courseName} </h1>
-            {renderVideo()}
-            <div className="collection">
+        <div className="container" >
+            {/* <h1> {courseName} </h1> */}
+            <div className="row">
+                <div className="col-12">
+                    {renderVideo()}
+                    <h3> {title}</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    {renderVideo()}
+                    <h3> {title}</h3>
+                </div>
+                <div className="col-6">
+                    {renderVideo()}
+                    <h3> {title}</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    {renderVideo()}
+                    <h3> {title}</h3>
+                </div>
+                <div className="col-6">
+                    {renderVideo()}
+                    <h3> {title}</h3>
+                </div>
+            </div>
+            <ul className="collection">
                 {
-                    courses[courseName].map((item) => {
-                        return <a href="#" className="collection-item" onClick={() => {
+                    courses.map((item, index) => {
+                        return <li key={index} href="#" className={counter === index ? "collection-item active" : "collection-item"} style={{ cursor: 'pointer' }} onClick={() => {
                             setTiTle(item.title);
                             setVid(item.vid);
-                        }}>{item.title}</a>
+                            setCounter(index)
+                        }}>{item.title}</li>
                     })
                 }
-            </div>
+            </ul>
         </div>
     );
 }
